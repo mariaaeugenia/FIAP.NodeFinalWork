e670cba91591f8e3db84f9d8d77ab4811751d3c0
Leandro Romano <leandroromano.dev@gmail.com>
2019-11-21 11:24:35 -0300

    Move auth endpoint to router entrypoint.

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const app = express();
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
-const authController = require('./controllers/authController')
-app.post('/auth', authController.auth)
-
 const routes = require('./routes')
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const users = require('./users');
 const products = require('./products');
+const auth = require('./auth');
 const verifyToken = require('../middlewares/verifyToken');
 const router = express.Router();
 
 router.use('/users', verifyToken, users);
 router.use('/products', verifyToken, products);
+router.use('/auth', auth);
 
 module.exports = router
