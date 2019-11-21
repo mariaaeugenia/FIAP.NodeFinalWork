98491a67dedae65ff3e6b53b1639b047a978ac07
Leandro Romano <leandroromano.dev@gmail.com>
2019-11-20 23:16:17 -0300

    Fix create token function to return the created token.

--- utils/createToken.js
+++ utils/createToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { secret } = require('../config/default');
 
 module.exports = (data, expiresIn = 300) => {
-    jwt.sign(
+    return jwt.sign(
         data,
         secret,
         { expiresIn }
