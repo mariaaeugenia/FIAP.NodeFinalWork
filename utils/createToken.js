095cc83532c9d345b97a04473f1d4c1c8e899319
Leandro Romano <leandroromano.dev@gmail.com>
2019-11-20 23:19:51 -0300

    Increase token lifetime from 5 minutes to 12 hours.

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { secret } = require('../config/default');
 
-module.exports = (data, expiresIn = 300) => {
+module.exports = (data, expiresIn = 43200) => {
     return jwt.sign(
         data,
         secret,
