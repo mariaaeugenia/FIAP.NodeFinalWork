166e5a855ecbfb636785eab82f6e24adebd6336c
Leandro Romano <leandroromano.dev@gmail.com>
2019-11-20 23:00:48 -0300

    Added more configurations for application entrypoint.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const authController = require('./controllers/authController')
+app.post('/auth', authController.auth)
+
+const routes = require('./routes')
+app.use(routes)
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
-});
-
-app.get('/produtos/:id', (request, response) => {
-    const { id } = request.params;
-
-    response.status(200).send({
-        id
-    });
 });
