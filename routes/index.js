78265592eda279077c4c9751ea57d9dc42a03a62
Leandro Romano <leandroromano.dev@gmail.com>
2019-11-20 23:01:30 -0300

    Fix verifyToken middleware missing reference for routes.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const users = require('./users')
 const router = express.Router();
 
+const verifyToken = require('../middlewares/verifyToken');
+
 router.use('/users', verifyToken, users);
 
 module.exports = router
