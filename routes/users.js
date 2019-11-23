1eadc30ee5c8f2f6f5f2405538b3c9a1014aac61
Maria Eugênia <mariaeugenia.dev@gmail.com>
2019-11-23 11:19:14 -0300

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/UsersController')
+const controller = require('../controllers/usersController')
 const verifyToken = require('../middlewares/verifyToken');
 
 router.get('/', verifyToken, controller.getAll);
